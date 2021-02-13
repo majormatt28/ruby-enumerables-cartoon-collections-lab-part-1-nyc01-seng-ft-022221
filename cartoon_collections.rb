6e1048c27becd27e0bfb5780b5b2da26f71cf5e0
Mathew E Newton <elijah2895@hotmail.com>
2021-02-13 02:28:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each { |characters| String == "Hello #{characters} !" }
+  array.each { |characters| "Hello #{characters} !" }
 end
 
 def list_dwarves(array)
